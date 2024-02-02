f97b76a4c883d1e02598b79e462118c5d38fec2e
Kyle Ferguson <kferguson@xoverlogin2.lcrc.anl.gov>
2024-02-02 10:19:03 -0600

    new method of calculating spanning tree weight, as well as an alternative method of calculating the true max spanning tree

--- src/m2stitch/_global_optimization.py
+++ src/m2stitch/_global_optimization.py
@@ -2,8 +2,13 @@ import networkx as nx
 import numpy as np
 import pandas as pd
 
-from ._typing_utils import Int
+from tqdm import tqdm
+from ._typing_utils import Int, Float, NumArray
+from ._translation_computation import extract_overlap_subregion, ncc
+from copy import deepcopy
 
+global ncc_dict
+ncc_dict = {'left':{}, 'top':{}}
 
 def compute_maximum_spanning_tree(grid: pd.DataFrame) -> nx.Graph:
     """Compute the maximum spanning tree for grid position determination.
@@ -24,8 +29,8 @@ def compute_maximum_spanning_tree(grid: pd.DataFrame) -> nx.Graph:
         for direction in ["left", "top"]:
             if not pd.isna(g[direction]):
                 weight = g[f"{direction}_ncc"]
-                if g[f"{direction}_valid3"]:
-                    weight = weight + 10
+                #if g[f"{direction}_valid3"]:
+                #    weight = weight + 10
                 connection_graph.add_edge(
                     i,
                     g[direction],
@@ -37,6 +42,87 @@ def compute_maximum_spanning_tree(grid: pd.DataFrame) -> nx.Graph:
                     x=g[f"{direction}_x"],
                 )
     return nx.maximum_spanning_tree(connection_graph)
+
+
+def full_tree_weight(grid: pd.DataFrame, images: NumArray, add: Bool = True) -> Float:
+    def overlap_ncc(params, im1, im2):
+        y, x = params
+        subI1 = extract_overlap_subregion(im1, y, x)
+        subI2 = extract_overlap_subregion(im2, -y, -x)
+        if subI1.shape[0] == 0 or subI2.shape[0] == 0:
+            return np.nan
+        return ncc(subI1, subI2)
+
+    if add:
+        total_ncc = 0
+    else:
+        total_ncc = 1
+
+    for direction in ["left", "top"]:
+        for i2, g2 in grid.iterrows():
+            i1 = g2[direction]
+            if pd.isna(i1):
+                continue
+
+            if i2 not in ncc_dict[direction]:
+                ncc_dict[direction][i2] = {}
+
+            g1 = grid.iloc[i1]
+            image1 = images[i1]
+            image2 = images[i2]
+            sizeY, sizeX = image1.shape
+
+            y_overlap = g2["y_pos"] - g1["y_pos"]
+            x_overlap = g2["x_pos"] - g1["x_pos"]
+
+            if (y_overlap, x_overlap) not in ncc_dict[direction][i2]:
+                ncc_dict[direction][i2][(y_overlap, x_overlap)] = \
+                    overlap_ncc([y_overlap, x_overlap], image1, image2)
+
+            if add:
+                total_ncc += ncc_dict[direction][i2][(y_overlap, x_overlap)]
+            else:
+                total_ncc *= ncc_dict[direction][i2][(y_overlap, x_overlap)]
+    return total_ncc
+
+
+def alternative_max_spanning_tree(grid: pd.DataFrame, images: NumArray, max_iterations: Int = 100000) -> nx.Graph:
+    connection_graph = nx.Graph()
+    for i, g in grid.iterrows():
+        for direction in ["left", "top"]:
+            if not pd.isna(g[direction]):
+                weight = g[f"{direction}_ncc"]
+                #if g[f"{direction}_valid3"]:
+                #    weight = weight + 10
+                connection_graph.add_edge(
+                    i,
+                    g[direction],
+                    weight=weight,
+                    direction=direction,
+                    f=i,
+                    t=g[direction],
+                    y=g[f"{direction}_y"],
+                    x=g[f"{direction}_x"],
+                )
+    max_ncc = 0
+    max_tree = None
+    max_it = None
+    all_trees = nx.SpanningTreeIterator(connection_graph, weight='weight', minimum=False)
+
+    nccs = []
+    for it, s in tqdm(enumerate(all_trees)):
+        # iterate over all spanning trees, from maximum default weight to minimum
+        new_grid = compute_final_position(grid, s)
+        ncc = full_tree_weight(new_grid, images)
+        nccs.append(ncc)
+        if ncc > max_ncc:
+            max_ncc = deepcopy(ncc)
+            max_tree = deepcopy(s)
+            max_it = deepcopy(it)
+        if it > max_iterations:
+            break
+
+    return max_tree, nccs
 
 
 def compute_final_position(
